01bdbcd0605e32a7e46f508a6cf791b9229364a6
Danu Wardana <74839306+kryptonxarmy@users.noreply.github.com>
2025-08-07 16:12:09 +0700

    fixed type error

--- src/lib/dndUtils.ts
+++ src/lib/dndUtils.ts
@@ -1,7 +1,7 @@
 // Drag and drop utility for status change
 export const statuses = ["todo", "inprogress", "done"] as const;
 
-export function getNextStatus(current: string) {
+export function getNextStatus(current: typeof statuses[number]) {
   const idx = statuses.indexOf(current);
   return statuses[(idx + 1) % statuses.length];
 }
